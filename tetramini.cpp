b63c74bdef8e5a250343c093d4c5707f8cd79d4d
Lory <lorenzo.dazzi@studio.unibo.it>
2024-06-01 16:16:50 +0200

    Aggiunti tutti gli algoritmi necessari allo spostamento e rotazione del tetramino

--- tetramini.cpp
+++ tetramini.cpp
@@ -33,7 +33,7 @@ class tetramino {
 
         tetramino(int colour) {
             this->colour = colour;
-            rot_id = 1;
+            rot_id = 0;
 
             switch (colour){
             
@@ -212,7 +212,6 @@ class tetramino {
                 break;
             }
         }
-        /*Codice di tankest2015*/
 
         bool control_rot(char C){       // l: for left
             Position P1, P2, P3, P4;    // r: for right
@@ -226,34 +225,34 @@ class tetramino {
             if(C == 'r'){
                 switch (colour){
                 case 1:                 //Tetramino: Z
-                    if(rot_id == 1){
+                    if(rot_id == 0){
                         P1.y --;
                         P3.x --;
                         P4.y ++;
                         P4.x --;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.x --;
                         P3.y ++;
                         P4.x ++;
                         P4.y ++;
-                        rot_id = 1;
+                        rot_id = 0;
                     }
                     break;
 
                 case 2:                 //Tetramino: S
-                    if(rot_id == 1){
+                    if(rot_id == 0){
                         P1.y --;
                         P3.x ++;
                         P4.y ++;
                         P4.x ++;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.x --;
                         P3.y --;
                         P4.x ++;
                         P4.y --;
-                        rot_id = 1;
+                        rot_id = 0;
                     }
                     break;
 
@@ -262,87 +261,87 @@ class tetramino {
                     break;
 
                 case 4:                 //Tetramino: J
-                    if(rot_id == 1){
+                    if(rot_id == 0){
                         P1.x ++;
                         P3.y --;
-                        P4.y = P4.y - 2;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        P4.y -= 2;
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.y ++;
                         P3.x --;
-                        P4.x = P4.x - 2;
-                        rot_id = 3;
-                    } else if(rot_id == 3){
+                        P4.x -= 2;
+                        rot_id = 2;
+                    } else if(rot_id == 2){
                         P1.x --;
                         P3.y --;
-                        P4.y = P4.y - 2;
-                        rot_id = 4;
-                    } else if(rot_id == 4){
+                        P4.y -= 2;
+                        rot_id = 3;
+                    } else if(rot_id == 3){
                         P1.y --;
                         P3.x ++;
-                        P4.x = P4.x + 2;
-                        rot_id = 1;
+                        P4.x += 2;
+                        rot_id = 0;
                     }
                     break;
 
                 case 5:                 //Tetramino: T
-                    if(rot_id == 1){
+                    if(rot_id == 0){
                         P1.y --;
                         P3.x ++;
                         P4.y ++;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.x ++;
                         P3.y ++;
                         P4.y --;
-                        rot_id = 3;
-                    } else if(rot_id == 3){
+                        rot_id = 2;
+                    } else if(rot_id == 2){
                         P1.y ++;
                         P3.x --;
                         P4.y --;
-                        rot_id = 4;
-                    } else if(rot_id == 4){
+                        rot_id = 3;
+                    } else if(rot_id == 3){
                         P1.x --;
                         P3.y --;
                         P4.x ++;
-                        rot_id = 1;
+                        rot_id = 0;
                     }
                     break;
                 case 6:                 //Tetramino: I
-                if(rot_id == 1){
+                if(rot_id == 0){
                         P1.x ++;
                         P3.x --;
-                        P4.x = P4.x - 2;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        P4.x -= 2;
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.y --;
                         P3.y ++;
-                        P4.y = P4.y + 2;
-                        rot_id = 1;
+                        P4.y += 2;
+                        rot_id = 0;
                     }
                     break;
 
                 case 7:                 //Tetramino: L
-                    if(rot_id == 1){
+                    if(rot_id == 0){
                         P1.x ++;
                         P3.y --;
-                        P4.y = P4.y - 2;
-                        rot_id = 2;
-                    } else if(rot_id == 2){
+                        P4.y -= 2;
+                        rot_id = 1;
+                    } else if(rot_id == 1){
                         P1.y ++;
                         P3.x ++;
-                        P4.x = P4.x + 2;
-                        rot_id = 3;
-                    } else if(rot_id == 3){
+                        P4.x += 2;
+                        rot_id = 2;
+                    } else if(rot_id == 2){
                         P1.x --;
                         P3.y ++;
-                        P4.y = P4.y + 2;
-                        rot_id = 4;
-                    } else if(rot_id == 4){
+                        P4.y += 2;
+                        rot_id = 3;
+                    } else if(rot_id == 3){
                         P1.y --;
                         P3.x --;
-                        P4.x = P4.x - 2;
-                        rot_id = 1;
+                        P4.x -= 2;
+                        rot_id = 0;
                     }
                     break;
                 
@@ -353,34 +352,154 @@ class tetramino {
             }
             else if(C == 'l'){
                 switch (colour){
-                case 1:                 //Tetramino: Z
+                case 1:                 //Tetramino: O
+                //change pointer order during tetramino creation, so assuming p1 is always the pivot
+                    if(rot_id == 0) {
+                        p1.y++;                 //                      [p4]
+                        p3.x++;                 //[p1][p2]          [p2][p3]
+                        p4.x++;                 //    [p3][p4]  ->  [p1]
+                        p4.y++;
+                        rot_id = 1;
+                    }
+                    if(rot_id == 1) {
+                        p1.x--;                 //    [p4]          [p1][p2]
+                        p3.y++;                 //[p2][p3]      ->      [p3][p4]
+                        p4.x++;                 //[p1]
+                        p4.y++;
+                        rot_id = 0;
+                    }
                     break;
 
                 case 2:                 //Tetramino: S
+                    if(rot_id == 0) {
+                        p1.y++;                 //      [p3][p4]    [p4]
+                        p3.x--;                 //  [p1][p2]    ->  [p3][p2]
+                        p4.x--;                 //                      [p1]
+                        p4.y--;
+                        rot_id = 1;
+                    }
+                    if(rot_id == 1) {
+                        p1.x--;                 //[p4]                [p3][p4]
+                        p3.y--;                 //[p3][p2]      ->[p1][p2]
+                        p4.x++;                 //    [p1]
+                        p4.y--;
+                        rot_id = 0;
+                    }
                     break;
 
-                case 3:                 //Tetramino: J
+                case 3:                 //Tetramino: O
+                //no need to rotate
                     break;
 
-                case 4:                 //Tetramino: O
+                case 4:                 //Tetramino: J
+                    if(rot_id == 0) {		    //			              [p4]
+                        p2.x++;			        //[p1]			      	  [p3]
+                        p3.y++;			        //[p2][p3][p4]   ->   [p1][p2]
+                        p4.x++;
+                        p4.y+=2;
+                        rot_id = 3;
+                    }
+                    else if(rot_id == 1) {		//[p2][p1]
+                        p2.y++;			        //[p3]		      [p1]
+                        p3.x+=2;		        //[p4]		 ->   [p2][p3][p4]
+                        p4.x+=2;
+                        p4.y--;
+                        rot_id = 0;
+                    }
+                    else if(rot_id == 2) {		//[p4][p3][p2]	      [p2][p1]
+                        p2.y++;			        //        [p1]   ->   [p3]
+                        p3.x++;			        //		              [p4]
+                        p3.y++;
+                        p4.x+=2;
+                        p4.y++;
+                        rot_id = 1;
+                    }
+                    else if(rot_id == 3) {		//    [p4]
+                        p2.y--;			        //    [p3]	        [p4][p3][p2]
+                        p3.x--;			        //[p1][p2]       ->         [p1]
+                        p3.y--;
+                        p4.x-=2;
+                        p4.y--;
+                        rot_id = 2;
+                    }
                     break;
 
                 case 5:                 //Tetramino: T
+                    if(rot_id == 0) {
+                        p1.y++;                 //                  [p3]
+                        p3.y--;                 //[p1][p2][p3]  ->  [p2][p4]
+                        p4.x++;                 //    [p4]          [p1]
+                        rot_id = 3;
+                    }
+                    if(rot_id == 1) {
+                        p1.x--;                 //    [p1]
+                        p3.x++;                 //[p4][p2]      ->  [p1][p2][p3]
+                        p4.y++;                 //    [p3]              [p4]
+                        rot_id = 0;
+                    }
+                    if(rot_id == 2) {
+                        p1.y--;                 //    [p4]            [p1]
+                        p3.y++;                 //[p3][p2][p1]  ->[p4][p2]
+                        p4.x--;                 //                    [p3]
+                        rot_id = 1;
+                    }
+                    if(rot_id == 3) {
+                        p1.x++;                 //[p3]                 [p4]
+                        p3.x--;                 //[p2][p4]      -> [p3][p2][p1]
+                        p4.y--;                 //[p1]
+                        rot_id = 2;
+                    }
                     break;
+
                 case 6:                 //Tetramino: I
+                    if(rot_id == 0) {           //                      [p1]
+                        p1.y--;                 //                      [p2]
+                        p3.y++;                 //[p1][p2][p3][p4]  ->  [p3]
+                        p4.y+=2;                //                      [p4]
+                        rot_id = 1;
+                    }
+                    if(rot_id == 1) {           //[p1]
+                        p1.x--;                 //[p2]
+                        p3.x++;                 //[p3]      ->  [p1][p2][p3][p4]
+                        p4.x+=2;                //[p4]
+                        rot_id = 0;
+                    }
                     break;
 
                 case 7:                 //Tetramino: L
+                    if(rot_id == 0) {           //        [p1]
+                        p1.x--;                 //[p4][p3][p2]   ->   [p1][p2]
+                        p3.y++;                 //                        [p3]
+                        p4.y+=2;                //                        [p4]
+                        rot_id = 3;
+                    }
+                    if(rot_id == 1) {
+                        p1.y--;                 //[p4]                      [p1]
+                        p3.x--;                 //[p3]          ->  [p4][p3][p2]
+                        p4.x-=2;                //[p2][p1]
+                        rot_id = 0;
+                    }
+                    if(rot_id == 2) {           //                  [p4]
+                        p1.x++;                 //                  [p3]
+                        p3.y--;                 //[p2][p3][p4]  ->  [p2][p1]
+                        p4.y-=2;                //[p1]
+                        rot_id = 1;
+                    }
+                    if(rot_id == 3) {
+                        p1.y++;                 //[p1][p2]      ->  [p2][p3][p4]
+                        p3.x++;                 //    [p3]          [p1]
+                        p4.x+=2;                //    [p4]
+                        rot_id = 2;
+                    }
                     break;
                 
                 default:
-
                     break;
                 }
             }
-                
+    
             /*Control if the blocks are in the grid and can rotate without collide*/
-            if(P1.x < 10 && P2.x < 10 && P3.x < 10 && P4.x < 10 && P1.x > 0 && P2.x > 0 && P3.x > 0 && P4.x > 0 && P1.x < 21 && P2.x < 21 && P3.x < 21 && P4.x < 21){
+            if(P1.x < 10 && P2.x < 10 && P3.x < 10 && P4.x < 10 && P1.x > 0 && P2.x > 0 && P3.x > 0 && P4.x > 0 && P1.y < 21 && P2.y < 21 && P3.y < 21 && P4.y < 21){
                 if(Board::matrix[p1.x][p1.y] != 1 && Board::matrix[p2.x][p2.y] != 1 && Board::matrix[p3.x][p3.y] != 1 && Board::matrix[p4.x][p4.y] != 1){
                     p1 = P1;
                     p2 = P2;
@@ -392,113 +511,20 @@ class tetramino {
             return false;
         }
 
-        bool rot_right (){
+        void left_rotation() {				//ogni volta che si genera un tetramino si impone rot_id == 0
+        board_delete_assign(false);
+        
+        control_rot('l');
+
+        board_delete_assign(true);
+        }
+
+        void right_rotation(){
             board_delete_assign(false);
 
             control_rot('r');
 
             board_delete_assign(true);
-            
-            switch (colour){
-            
-            case 1:
-                break;
-
-             case 2:
-                break;
-
-            case 3: 
-                break;
-
-            case 4:
-                break;
-
-            case 5:
-                break;
-            case 6: //Pivot -> p2
-                p1 = p2;
-                p3 = p2;
-                p4 = p2;
-
-                if(rot.x == -1 && rot.y == -1){
-                    rot.y = +2;
-                    sign = +1;
-                }
-                else if(rot.x == -1 && rot.y == +1){
-                    rot.x = +2;
-                    rot.y = +1;
-                    sign = -1;
-                }
-                else if(rot.x == +1 && rot.y == +1){
-                    rot.y = -2;
-                    sign = -1;
-                }
-                else if(rot.x == +1 && rot.y == -1){
-                    rot.x = -2;
-                    rot.y = -1;
-                    sign = +1;
-                }
-
-                if((rot.x > 0 && rot.y > 0) || (rot.x < 0 && rot.y < 0)){
-                    p1.x = p1.x + sign;
-                    p3.y = p3.x + rot.y;
-                    p4.x = p4.x + rot.x;
-                    rot.x = rot.y;
-                }
-                else{
-                    p1.y = p1.y + rot.x;
-                    p3.y = p3.y + sign;
-                    p4.y = p4.y + rot.y;
-                    rot.y = sign;
-                }
-                break;
-
-            case 7:     // Pivot -> p2
-                p1 = p2;
-                p3 = p2;
-                p4 = p2;
-
-                if(rot.x == -1 && rot.y == -1){
-                    rot.x = +1;
-                    rot.y = -2;
-                    sign = -1;
-                }
-                else if(rot.x == +1 && rot.y == -1){
-                    rot.x = +2;
-                    rot.y = +1;
-                    sign = +1;
-                }
-                else if(rot.x == +1 && rot.y == +1){
-                    rot.x = -1;
-                    rot.y = +2;
-                    sign = +1;
-                }
-                else if(rot.x == -1 && rot.y == +1){
-                    rot.x = -2;
-                    rot.y = -1;
-                    sign = -1;
-                }
-
-                if(ABS(rot.x) > ABS(rot.y)){
-                    p1.y = p1.y + rot.y;
-                    p3.x = p3.x + sign;
-                    p4.x = p3.x + rot.x;
-                    rot.x = sign;
-                }
-                else{
-                    p1.x = p1.x + rot.x;
-                    p3.y = p3.y + sign;
-                    p4.y = p4.y + rot.y;
-                    rot.y = sign;
-                }
-                break;
-            
-            default:
-
-                break;
-            }
-
-            return true;
         }
 
         bool move_right(){
@@ -550,10 +576,10 @@ tetramino* gen_tetramino(){
     return point;
 }
 
-int ABS(int x){
+/*int ABS(int x){
     if(x<0) x = x*-1;
     return x;
-}
+}*/
 
 void board_delete_assign (bool action){     // For delete use false
     if(action){                             // For assign use true
@@ -569,28 +595,6 @@ void board_delete_assign (bool action){     // For delete use false
         Board::matrix[p4.x][p4.y] = 0;
     }
 }
-
-
-/*void move_left(tetramino* point){ //SERVE LA GRIGLIA
-    if(point->p1.x == 1 || point->p2.x == 1 || point->p3.x == 1 || point->p4.x == 1) return;
-
-}
-
-bool check_move(char c, tetramino* point)
-{
-    switch (c)
-    {
-    case 'l':
-        if(point->p1.x == 1 || point->p2.x == 1 || point->p3.x == 1 || point->p4.x == 1) return false;
-        break;
-    
-    case 'r':
-        if(point->p1.x == 10 || point->p2.x == 10 || point->p3.x == 10 || point->p4.x == 10) return false;
-
-    default:
-        break;
-    }  
-}*/
 
 
 int main() {
